4bd56beccf8c77ea2e38ed85ead89396c1d53092
Mike B <115735557+mbartnett@users.noreply.github.com>
2023-01-18 00:41:45 -0600

    fixed a bug after tutoring

--- index.js
+++ index.js
@@ -4,30 +4,32 @@ const inquirer = require('inquirer')
 const mysql = require('mysql2')
 const consoleTable = require('console.table')
 
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
+const db = mysql.createConnection({
+        host: '127.0.0.1',
         port: 3306,
         user: 'root',
         password: '',
         database: 'employees_db',
-    }
-)
+})
 
-db.connect(() => {
+db.connect((err) => {
+    // if (err) throw err
+    if(err) {
+        return console.log('error:' + err.message)
+    }
     console.log(`Connected to the employee database.`)
     init()
 }) 
-
-// db.query('SELECT * FROM employees', function (err, results) {
-//     console.log(results);
-// })
 
 //  ___               _                        __  __                                   
 // | __| _ __   _ __ | | ___  _  _  ___  ___  |  \/  | __ _  _ _   __ _  __ _  ___  _ _ 
 // | _| | '  \ | '_ \| |/ _ \| || |/ -_)/ -_) | |\/| |/ _` || ' \ / _` |/ _` |/ -_)| '_|
 // |___||_|_|_|| .__/|_|\___/ \_, |\___|\___| |_|  |_|\__,_||_||_|\__,_|\__, |\___||_|  
-//             |_|            |__/                                      |___/           
+//             |_|            |__/                                      |___/         
+
+// db.query('SELECT * FROM employees', function (err, results) {
+//     console.log(results);
+// })
 
 // Questions
 
@@ -36,21 +38,9 @@ const questions = [
 type: "list",
 name: "Selection",
 message: "What would you like to do?",
-choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role",
- "View All Departments", "Add Department", "Quit"],
+choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"],
     }
 ]
-
-// "What would you like to do?"
-
-// "View All Employees",
-// "Add Employee",
-// "Update Employee Role",
-// "View All Roles"
-// "Add Role",
-// "View All Departments",
-// "Add Department",
-// "Quit"
 
 // "What is the name of the department?"
 // "What is the name of the role?"
@@ -58,15 +48,7 @@ choices: ["View All Employees", "Add Employee", "Update Employee Role", "View Al
 // "What is the employee's last name?"
 // "What is the employee's role?"
 // "Who is the employee's manager?"
-
 // "Which employee's role do you want to update?"
-
-function viewAllDepartments(){
-    db.query('SELECT * FROM DEPARTMENT', (err, data) => {
-        console.table(data)
-        init()
-    })
-}
 
 function init() {
     inquirer.prompt(questions).then(response =>{
@@ -76,3 +58,9 @@ function init() {
 })
 }
 
+function viewAllDepartments(){
+    db.query(`SELECT * FROM department`, (err, data) => {
+        console.table(data)
+        init()
+    })
+}
